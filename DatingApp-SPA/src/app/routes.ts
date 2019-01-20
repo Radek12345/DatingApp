7f3a661250a26d4f848ae4806de53ef172788677
Radek12345 <gosciminskiradoslaw@gmail.com>
2019-01-20 19:21:15 +0100

    End of section 9

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,13 +1,17 @@
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MessagesComponent } from './messages/messages.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+    { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}, canActivate: [AuthGuard]},
+    { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}, canActivate: [AuthGuard]},
     { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
     { path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
     { path: '**', redirectTo: '', pathMatch: 'full'}
